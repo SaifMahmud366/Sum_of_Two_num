33e38d0d3056f9b9d674485f02040fecbbc37b09
Saif Mahmud <46050030+SaifMahmud366@users.noreply.github.com>
2022-11-19 20:16:55 +0600

    class removed & new code updated

--- main.cpp
+++ main.cpp
@@ -1,25 +1,23 @@
-#include<iostram>
-using nameapace std;
-class main
-{
-  public:
-  double x,n,z;
-  void in()
-  {
-    cout<<"Enter First Number";
-    cin>>x;
-    cout<<"Enter Second Number";
-    cin>>n;
-  }
-  void run
-  {
-    z=x+n;
-    cout<<"Summation= "<<z;
-  }
-};
+#include<iostream>
+using namespace std;
 int main()
 {
-  main obj;
-  obj.in();
-  obj.run();
+    double a,b,c;
+    char x;
+    do
+    {
+        cout<<endl<<endl<<"Summation of Two number"<<endl;
+        cout<<"Enter 1st Number= ";
+        cin>>a;
+        cout<<"Enter 2nd Number= ";
+        cin>>b;
+        c=a+b;
+        cout<<"Summation= "<<c<<endl;
+        cout<<endl<<"Wanna try again Y/N ?"<<endl<<"=> ";
+        cin>>x;
+        if(x=='N' || x=='n')
+        {
+            break;
+        }
+    } while (x=='Y' || x=='y');
 }
